eae43353500f077fc1d16248cbd83f2cdb70618e
andresgz <andresgz@gmail.com>
2015-11-17 07:10:27 -0500

    Update version on setup to 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def readme():
         return f.read()
 
 setup(name='python-teamwork',
-      version='0.1.0',
+      version='0.1.1',
       description='Python Wrapper for Teamwork API',
       long_description=readme(),
       url='https://github.com/andresgz/python-teamwork',
