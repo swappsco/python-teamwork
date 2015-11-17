db4db1560a110c43e8f974db785eb58c64d8edc4
andresgz <andresgz@gmail.com>
2015-11-17 06:36:04 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='python-teamwork',
+      version='0.1.0',
+      description='Python Wrapper for Teamwork API',
+      url='https://github.com/andresgz/python-teamwork',
+      author='Andres Gonzalez',
+      author_email='andresgz@gmail.com',
+      license='MIT',
+      packages=['python-teamwork'],
+      zip_safe=False)
